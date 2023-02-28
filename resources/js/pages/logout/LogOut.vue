18d335f0496486f30dea241267bf5fd3fbcf6a60
sohan065 <mdsohan065@gmail.com>
2023-02-28 18:50:44 +0600

    vee-validation added

--- resources/js/pages/logout/LogOut.vue
+++ resources/js/pages/logout/LogOut.vue
@@ -7,13 +7,33 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item ">Home<a href="http://" target="_blank" rel="noopener noreferrer"></a></li>
-                        <li class="breadcrumb-item active">Log Out</li>
+                        <li class="breadcrumb-item">
+                            Home<a
+                                href="http://"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            ></a>
+                        </li>
                     </ol>
                 </div>
             </div>
         </div>
     </div>
+    <div id="info">
+        <Form @submit="onSubmit" :validation-schema="schema">
+            <Field name="email" type="email" />
+            <button>Sign up for newsletter</button>
+        </Form>
+    </div>
 </template>
 
-<script></script>
+<script setup>
+import { Form, Field } from "vee-validate";
+import * as yup from "yup";
+const onSubmit = (values) => {
+    console.log(values);
+};
+const schema = yup.object().shape({
+    email: yup.string().email().required(),
+});
+</script>
